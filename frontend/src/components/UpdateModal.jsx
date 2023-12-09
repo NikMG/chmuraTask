ef2374274098e2fea0c7e224f57e3217eb293ed6
Nykyta <nikita.mg2@gmail.com>
2023-12-09 15:35:12 +0100

    commit

--- frontend/src/components/UpdateModal.jsx
+++ frontend/src/components/UpdateModal.jsx
@@ -10,7 +10,7 @@ const UpdateModal = ({ isOpen, setOpen, activeTask, fetchTasks }) => {
     }
 
     let handleUpdate = async () => {
-        await axios.put('http://127.0.0.1:3001/tasks/update', {
+        await axios.put('http://16.170.231.118:3001/tasks/update', {
             taskId: activeTask.id,
             text: taskInput
         });
